9045068d3d0693f7b33481cc4742f66b94ab4092
Jan Range <range.jan@web.de>
2024-06-04 14:12:14 +0200

    add generate binding

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,27 @@ pub(crate) mod markdown {
     pub(crate) mod frontmatter;
     pub(crate) mod parser;
 }
+
+use exporters::Templates;
+use markdown::parser::parse_markdown;
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn generate(schema: &str, template: Templates) -> PyResult<String> {
+    let mut model = parse_markdown(schema).expect("Could not parse markdown");
+    let code = exporters::render_jinja_template(&template, &mut model);
+
+    match code {
+        Ok(code) => Ok(code),
+        Err(e) => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            e.to_string(),
+        )),
+    }
+}
+
+#[pymodule]
+fn mdmodels(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(generate, m)?)?;
+    m.add_class::<Templates>()?;
+    Ok(())
+}
